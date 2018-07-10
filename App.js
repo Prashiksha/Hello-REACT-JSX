9fd686262f624176083f89236adb1dfa0c19ce6f
Prashiksha Jain <35810637+Prashiksha@users.noreply.github.com>
2018-07-10 17:10:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>HELLO NPM</h1> 
+        <h2>Modified using Create-React-App</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
